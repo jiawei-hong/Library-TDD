043668cd107383d6219af72f30f82c4a2cd8a89d
jiawe-hong <zxc37984@gmail.com>
2021-07-25 12:03:47 +0800

    Remove default test

--- tests/Feature/PublishTest.php
+++ tests/Feature/PublishTest.php
@@ -8,15 +8,5 @@ use Tests\TestCase;
 
 class PublishTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    public function test_example()
-    {
-        $response = $this->get('/');
 
-        $response->assertStatus(200);
-    }
 }
